14e436fa8967419deb618cc360a44b38c40c037f
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-08-04 16:06:35 +0200

    DB Fix

--- includes/functions/article/db.inc.php
+++ includes/functions/article/db.inc.php
@@ -1,9 +1,16 @@
 <?php
 
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "fontanakupatila";
+    // Development Database
+    // $hostname = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $database = "fontanakupatila";
+
+    // Remote Database
+    $hostname = "remotemysql.com";
+    $username = "nUKA0UyNf0";
+    $password = "aIoFRaPUBT";
+    $database = "nUKA0UyNf0";
 
     $conn = mysqli_connect($hostname, $username, $password, $database);
     if(!$conn){
