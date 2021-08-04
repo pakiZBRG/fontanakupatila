6c94626528e55dd790f20e774206b84152d61933
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-08-04 14:47:08 +0200

    Day 11

--- includes/functions/filter_template.inc.php
+++ includes/functions/filter_template.inc.php
@@ -32,37 +32,42 @@
     mysqli_stmt_bind_param($stmt, 's', $name);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    while($row = mysqli_fetch_assoc($result)) {
-        $id = $row["product_id"];
-        $product_name = $row["product_name"];
-        $image = $row["image"];
-        $price = $row["price"];
-        $dimension = $row["dimension"];
-        $product_id = $row["product_id"];
-        $producer = $row["man_image"];
-        $squareM = "";
-        if($name == 'granit plocice' || $name == 'keramicke plocice' || $name === 'laminati' || $name == 'parket'){
-            $squareM = "/m²";
-        }
-
-        $format_price = number_format($price, 0, '', '.');
-        $link = strtolower(str_replace(" ", '-', $name));
-        $hasDimension = $dimension !== '0' ? "Dimenzija: $dimension mm" : "";
-
-        echo "
-            <div class='card'>
-                <a href='/fontanakupatila/kategorija/$link/$id'>
-                    <img src='/fontanakupatila/server/images/$image'/>
-                </a>
-                <div style='margin: 1.5rem'>
-                    <h3 class='name'>$product_name</h3>
-                    <h5 class='dimension'>$hasDimension</h5>
-                    <div class='producer'>
-                        <img class='image' src='/fontanakupatila/server/images/$producer'/>
+    $nums = mysqli_num_rows($result);
+    if($nums){
+        while($row = mysqli_fetch_assoc($result)) {
+            $id = $row["product_id"];
+            $product_name = $row["product_name"];
+            $image = $row["image"];
+            $price = $row["price"];
+            $dimension = $row["dimension"];
+            $product_id = $row["product_id"];
+            $producer = $row["man_image"];
+            $squareM = "";
+            if($name == 'granit plocice' || $name == 'keramicke plocice' || $name === 'laminati' || $name == 'parket'){
+                $squareM = "/m²";
+            }
+    
+            $format_price = number_format($price, 0, '', '.');
+            $link = strtolower(str_replace(" ", '-', $name));
+            $hasDimension = $dimension !== '0' ? "Dimenzija: $dimension mm" : "";
+    
+            echo "
+                <div class='card'>
+                    <a href='/fontanakupatila/kategorija/$link/$id'>
+                        <img src='/fontanakupatila/server/images/$image'/>
+                    </a>
+                    <div style='margin: 1.5rem'>
+                        <h3 class='name'>$product_name</h3>
+                        <h5 class='dimension'>$hasDimension</h5>
+                        <div class='producer'>
+                            <img class='image' src='/fontanakupatila/server/images/$producer'/>
+                        </div>
+                        <h4 class='price'>$format_price din$squareM</h4>
                     </div>
-                    <h4 class='price'>$format_price din$squareM</h4>
                 </div>
-            </div>
-        ";
+            ";
+        }
+    } else {
+        echo "<h1>Trenuto nema $name na stanju</h1>";
     }
 ?>
