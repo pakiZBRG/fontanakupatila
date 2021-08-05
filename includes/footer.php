3be10f3c5b996b86e796611723f852f75b639ed0
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-08-05 08:09:19 +0200

    Returning back the old footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,5 @@
         <footer class='footer'>
-            <div class='footer_up'>
-                <img src='/images/logo.png'/>
-                <ul class='footer_links'>
-                    <li><a href='/'>Pocetna</a></li>
-                    <li><a href='/saloni'>Saloni</a></li>
-                    <li><a href='/kontakt'>Kontakt</a></li>
-                    <li><a href='/o_nama'>O nama</a></li>
-                </ul>
-                <ul class='footer_links'>
-                    <li><i class='fa fa-map-marker'></i> Dušanova 56, Valjevo</li>
-                    <li><i class='fa fa-phone-square'></i> 014/238-123</li>
-                    <li><i class='fa fa-phone-square'></i> 014/232-906</li>
-                </ul>
-            </div>
-            <div class='footer_down'>
+            <div class='footer_flex'>
                 <p>Created by <a href='https://pakizbrg.com' target="blanc">pakiZBRG</a></p>
                 <p>Copyright © 2021 Fontana d.o.o.</p>
             </div>
